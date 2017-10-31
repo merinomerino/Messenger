72522d926b8ca2288128397538600904acd90205
Oswaldo Merino <32102477+merinomerino@users.noreply.github.com>
2017-10-30 21:05:34 -0600

    El socket del servidor y cliente
    El socket del servidor y del cliente

--- src/Server/Servidor.java
+++ src/Server/Servidor.java
@@ -7,6 +7,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Servidor  {
+	// El socket del servidor.
+    private static ServerSocket servidor = null;
+    
+// El socket del cliente.
+    private static Socket connection = null;
+
+    public static String mensagens = "";
+
+
+// Este servidor de chat puede aceptar hasta las conexiones de los clientes de maxClientsCount.
+    private static final int maxClientsCount = 10;
+    private static final ClienteThread[] threads = new ClienteThread[maxClientsCount];
+    
+     public Servidor(int porta, int tamFila) {
+
+        try {
+
+            servidor = new ServerSocket(porta, tamFila);
+
+        } catch (EOFException eof) {
+            System.out.println("\n Error de conexión con el cliente\n");
+            eof.printStackTrace();
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
