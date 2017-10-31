fc4b13d47a36c76fc5517408f5b9e2191d2b5368
Angel Abraham <32169462+AngelAbraham@users.noreply.github.com>
2017-10-30 19:27:20 -0600

    Agregacion de paquetes en clase cliente
    paquetes agregados

--- src/Client/Cliente.java
+++ src/Client/Cliente.java
@@ -1,10 +1,25 @@
 package Client;
-
-
-
-import javax.swing.*;
-
-
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 public class Cliente {
 
 	public static void main(String[] args) {
